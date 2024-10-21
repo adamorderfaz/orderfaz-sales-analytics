d3230a7dd68af7cd54cc4c7df1e1f3ae43141cef
adamorderfaz <adam.winata@orderfaz.com>
2024-10-21 15:39:18 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# ----- Import Library -----
+import pandas as pd
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+# ----- Load images as icon -----
+icon_image = Image.open("orderfaz.jpeg")
+
+# ----- Set Page Sidebar -----
+p1 = st.Page("pages/dashboard1.py", title="GMV Weekly", icon=":material/date_range:")
+p2 = st.Page("pages/dashboard2.py", title="GMV Monthly", icon=":material/calendar_month:")
+p3 = st.Page("pages/dashboard3.py", title="Coming Soon", icon=":material/hourglass_top:")
+
+# ----- Install Multi-page Navigation -----
+pg = st.navigation({
+    "Home": [st.Page("home.py", title="Home", icon=":material/home:")],
+    "Menu": [p1, p2, p3]
+})
+
+# ----- Set Page Config -----
+st.set_page_config(page_title="OF | Sales", page_icon=icon_image, layout="wide")
+
+# ----- Run Streamlit Page Navigation -----
+pg.run()
+
+# Share Info across all pages (optional)
+# ----- Set Logo -----
+st.logo('orderfaz.jpeg')
+st.sidebar.header(" ☎️ Adam Maurizio")
+
+
---
+++ home.py
@@ -0,0 +1,11 @@
+# ----- Import Library -----
+import streamlit as st
+
+# ----- Set Title -----
+st.title('Orderfaz Analytics - Sales Division')
+
+st.markdown('<div style="text-align: justify; font-size:160%"> Data Analytics untuk Sales ini merangkum berbagai macam feature yang berkaitan dengan requirements divisi Sales di tim Orderfaz. Data ini diproses secara real-time (24 jam sekali) untuk memastikan pengambilan insight dan keputusan bisnis yang lebih cepat. </div>',
+            unsafe_allow_html=True)
+st.markdown('<br>', unsafe_allow_html=True)
+st.warning('⏲️ Data di-Update setiap 24 jam sekali di 00.00')
+st.success('☎️ If you find any Bug/Issues, please contact Adam Maurizio at Google Chat')
